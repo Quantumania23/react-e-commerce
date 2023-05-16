f9802528562c9429b0d3d7bf1f24c281de1700d3
quantumania23 <116637399+Quantumania23@users.noreply.github.com>
2023-05-16 08:58:54 +0300

    first commit

---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from './assets/1.jpg';
+import product2 from './assets/2.jpg';
+import product3 from './assets/3.jpg';
+import product4 from './assets/3.jpg';
+import product5 from './assets/4.jpg';
+import product6 from './assets/5.jpg';
+import product7 from './assets/1.jpg';
+import product8 from './assets/1.jpg';
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: 'Iphone',
+        price: 999.0,
+        productImage: product1
+    },
+    {
+        id: 2,
+        productName: 'Macbook',
+        price: 1999.0,
+        productImage: product2
+    },
+    {
+        id: 3,
+        productName: 'Iphone',
+        price: 699.0,
+        productImage: product3
+    },
+    {
+        id: 4,
+        productName: 'Iphone',
+        price: 228.0,
+        productImage: product4
+    },
+    {
+        id: 5,
+        productName: 'Iphone',
+        price: 19.0,
+        productImage: product5
+    },
+    {
+        id: 6,
+        productName: 'Iphone',
+        price: 68.0,
+        productImage: product6
+    },
+    {
+        id: 7,
+        productName: 'Iphone',
+        price: 120.0,
+        productImage: product7
+    },
+    {
+        id: 8,
+        productName: 'Iphone',
+        price: 40.0,
+        productImage: product8
+    },
+]
