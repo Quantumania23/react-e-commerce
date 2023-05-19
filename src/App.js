45995a62a3f68c93e20f1ed22e6a4affeb67cdba
quantumania23 <116637399+Quantumania23@users.noreply.github.com>
2023-05-19 17:54:22 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Navbar }from './components/Navbar.js'
-import { Shop } from './pages/shop/shop.js'
-import { Cart } from './pages/cart/cart.js'
-import { ShopContextProvider } from './context/ShopContext';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/navbar";
+import { Shop } from "./pages/shop/shop";
+import { Contact } from "./pages/contact";
+import { Cart } from "./pages/cart/cart";
+import { ShopContextProvider } from "./context/shop-context";
+
 
 function App() {
+   
+
   return (
     <div className="App">
-      <ShopContextProvider/>
-     <Router>
-      <Navbar/>
-      <Routes>
-        <Route path ='/' element= {<Shop />}/>
-        <Route path ='/cart' element={<Cart />}/>
-      </Routes>
-     </Router>
+      <ShopContextProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Shop />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </Router>
+      </ShopContextProvider>
     </div>
   );
 }
